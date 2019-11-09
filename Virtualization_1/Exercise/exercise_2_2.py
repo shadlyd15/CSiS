1828155d05a96686f2095233889f33896585e53e
shadlyd15 <shadlyd15@gmail.com>
2019-11-09 15:59:16 +0100

    Virtualization 2.2 Refactored

--- Virtualization_1/Exercise/exercise_2_2.py
+++ Virtualization_1/Exercise/exercise_2_2.py
@@ -1,11 +1,7 @@
 import requests
-
-icao_code_repo =  {
-	"Dusseldorf" : "EDDL", "Frankfurt" : "EDDF", "Tokio" : "RJAA", "Buenos Aires" : "SAEZ", "Vancouver" : "CYVR", "Longyearbyen" : "ENSB", "Goose Bay" : "CYYR"
-}
+icao_code_repo = {"Dusseldorf" : "EDDL", "Frankfurt" : "EDDF", "Tokio" : "RJAA", "Buenos Aires" : "SAEZ", "Vancouver" : "CYVR", "Longyearbyen" : "ENSB", "Goose Bay" : "CYYR"}
 
 for city in icao_code_repo:
-	temp = 0
 	response = requests.get(url='https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=csv&stationString=' + icao_code_repo[city] + '&hoursBeforeNow=2')
 	temp = response.text.rsplit('/', 1)[0].rsplit(' ', 1)[1].replace('M', '-');
 	print("The temperature in " + city + "\tis : " + temp + " degrees C")
