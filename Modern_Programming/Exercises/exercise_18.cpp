d6293081e28c281d5d2e7ddee0d66dfdddb98adc
shadlyd15 <shadlyd15@gmail.com>
2019-11-22 22:37:53 +0100

    Exercise 18 Added

--- Modern_Programming/Exercises/exercise_18.cpp
+++ Modern_Programming/Exercises/exercise_18.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 class Averager{
 public:
-	Averager nextElement(double element);
+	Averager & nextElement(double element);
 	double getAverage();
 
 private:
@@ -17,7 +17,7 @@ private:
 	double total_elements;
 };
 
-Averager Averager::nextElement(double element){
+Averager & Averager::nextElement(double element){
 	this->total = this->total + element;
 	this->total_elements = this->total_elements + 1;
 	return (*this);
